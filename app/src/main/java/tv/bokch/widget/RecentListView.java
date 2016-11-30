6e128e00d35701a1e83c0ce9bb84980bf84bd2ce
takase.ryohei <takase.ryohei@donuts.ne.jp>
2016-11-30 16:46:33 +0900

    fix

--- app/src/main/java/tv/bokch/widget/RecentListView.java
+++ app/src/main/java/tv/bokch/widget/RecentListView.java
@@ -3,8 +3,6 @@ package tv.bokch.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import tv.bokch.R;
 import tv.bokch.data.History;
