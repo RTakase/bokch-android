5eef436abd6c892d9c5812d44a5f71f49755ec5e
takase.ryohei <takase.ryohei@donuts.ne.jp>
2016-12-29 10:58:13 +0900

    google analyticsの設定

--- app/src/main/java/tv/bokch/App.java
+++ app/src/main/java/tv/bokch/App.java
@@ -3,15 +3,24 @@ package tv.bokch;
 import android.app.Application;
 import android.content.SharedPreferences;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 import timber.log.Timber;
 import tv.bokch.data.User;
 
 public class App extends Application {
 	protected User myUser;
+	private Tracker mTracker;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		Timber.plant(new Timber.DebugTree());
+
+		GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+		mTracker = analytics.newTracker(R.xml.app_tracker);
+
 	}
 
 	public User getMyUser() {
@@ -24,5 +33,9 @@ public class App extends Application {
 			SharedPreferences pref = getSharedPreferences("bokch", MODE_PRIVATE);
 			pref.edit().putString("user_id", myUser.userId).apply();
 		}
+	}
+
+	public Tracker getTracker() {
+		return mTracker;
 	}
 }
